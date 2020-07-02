f7465d26c727cc3612f2d2c98ae4e1e714d7a3a1
xibhu <xibhuxan@gmail.com>
2020-07-03 00:07:52 +0100

    debounce and edge

--- Librerias/src/XibDebounceEdge/Example/Main.ino
+++ Librerias/src/XibDebounceEdge/Example/Main.ino
@@ -5,6 +5,7 @@ const int ledPin = 13;
 const int ledPin2 = 12; 
 const int ledPin3 = 11; 
 
+//Increase the "20" (milliseconds) if you still get false readings from the button
 XibDebounce myDebounce1(20);
 XibDebounce myDebounce2(20);
 XibDebounce myDebounce3(20);
