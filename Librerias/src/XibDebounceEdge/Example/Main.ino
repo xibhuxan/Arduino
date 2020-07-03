7d8c9b88136846a13e3932ee1eef08d3b505040f
xibhu <xibhuxan@gmail.com>
2020-07-03 13:02:45 +0100

    debounce and edge

--- Librerias/src/XibDebounceEdge/Example/Main.ino
+++ Librerias/src/XibDebounceEdge/Example/Main.ino
@@ -25,20 +25,24 @@ void setup()
 void loop()
 {
 
-  bool reading = !digitalRead(buttonPin);
+  bool reading = !digitalRead(buttonPin); //Read from the pin that have the button
 
+//while the input it's on, the function "whenActive" do the personal function
   myDebounce1.whenActive(reading, []() {
     digitalWrite(ledPin, 1);
   });
-
+  
+//while the input it's off, the function "whenDeactive" do the personal function
   myDebounce2.whenDeactive(reading, []() {
     digitalWrite(ledPin, 0);
   });
-
+  
+//Only in the rising edge, the personal function works (one time per rising edge)
   myDebounce3.whenRisingEdge(reading, []() {
     digitalWrite(ledPin2, led2State = !led2State);
   });
-
+  
+//Only in the falling edge, the personal function works (one time per falling edge)
   myDebounce4.whenFallingEdge(reading, []() {
     digitalWrite(ledPin3, led3State = !led3State);
   });
