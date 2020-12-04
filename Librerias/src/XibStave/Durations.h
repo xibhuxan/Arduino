710e57c0419c4a2e0d7900a46fb12a58662c3ec0
xibhu <xibhuxan@gmail.com>
2020-12-04 14:28:23 +0000

    added durations and commentaries

--- Librerias/src/XibStave/Durations.h
+++ Librerias/src/XibStave/Durations.h
@@ -1,17 +1,17 @@
-/*********************************************************************************************
- *  MUSIC DURATIONS      SPANISH                US                             UK               *
- *********************************************************************************************/
-const uint8_t NN = 0;   // NEGRA                 QUARTER NOTE                   CROTCHET
-const uint8_t NNP = 1;  // NEGRA CON PUNTILLO    QUARTER NOTE + EIGHTH NOTE     CROTCHET + QUAVER
-const uint8_t NR = 2;   // REDONDA               DOUBLE WHOLE NOTE              BREVE
-const uint8_t NRP = 3;  // REDONDA CON PUNTILLO
-const uint8_t NB = 4;   // BLANCA                HALF NOTE                      MINIM
-const uint8_t NBP = 5;  // BLANCA CON PUNTILLO
-const uint8_t NC = 6;   // CORCHEA               EIGHTH NOTE                    QUAVER
-const uint8_t NCP = 7;  // CORCHEA CON PUNTILLO
-const uint8_t NT = 8;   // TRESILLO                  --ONE THIRD OF QUARTER NOTE--
-const uint8_t NTP = 9;   // TRESILLO CON PUNTILLO
-const uint8_t NS = 10;  // SEMICORCHEA           SIXTEENTH NOTE                 SEMIQUAVER
-const uint8_t NSP = 11; // SEMICORCHEA CON PUNTILLO
-const uint8_t NF = 12;  // FUSA                 THIRTY-SECOND NOTE             DEMISEMIQUAVER
-const uint8_t NFP = 13; // FUSA CON PUNTILLO
+/*****************************************************************************************************
+ *  MUSIC DURATIONS             SPANISH                     US                              UK       *       
+ *****************************************************************************************************/
+const uint8_t NN    = 0;    // NEGRA                        QUARTER NOTE                    CROTCHET
+const uint8_t NNP   = 1;    // NEGRA CON PUNTILLO           QUARTER NOTE + 50%              CROTCHET + 50%
+const uint8_t NR    = 2;    // REDONDA                      DOUBLE WHOLE NOTE               BREVE
+const uint8_t NRP   = 3;    // REDONDA CON PUNTILLO         DOUBLE WHOLE NOTE + 50%         BREVE + 50%
+const uint8_t NB    = 4;    // BLANCA                       HALF NOTE                       MINIM
+const uint8_t NBP   = 5;    // BLANCA CON PUNTILLO          HALF NOTE + 50%                 MINIM + 50%
+const uint8_t NC    = 6;    // CORCHEA                      EIGHTH NOTE                     QUAVER
+const uint8_t NCP   = 7;    // CORCHEA CON PUNTILLO         EIGHTH NOTE + 50%               QUAVER + 50%
+const uint8_t NT    = 8;    // TRESILLO                         --ONE THIRD OF QUARTER NOTE--
+const uint8_t NTP   = 9;    // TRESILLO CON PUNTILLO
+const uint8_t NS    = 10;   // SEMICORCHEA                  SIXTEENTH NOTE                  SEMIQUAVER
+const uint8_t NSP   = 11;   // SEMICORCHEA CON PUNTILLO     SIXTEENTH NOTE + 50%            SEMIQUAVER + 50%
+const uint8_t NF    = 12;   // FUSA                         THIRTY-SECOND NOTE              DEMISEMIQUAVER
+const uint8_t NFP   = 13;   // FUSA CON PUNTILLO            THIRTY-SECOND NOTE + 50%        DEMISEMIQUAVER + 50%
